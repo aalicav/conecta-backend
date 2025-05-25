c2607cd8ecd9c292a964b99493807497690d7514
aalicav <antonioalissoncavalcantedeoliv@gmail.com>
2025-05-25 14:30:34 -0300

    Remove campos desnecessario da clinica

---
+++ database/migrations/2025_05_25_172208_remove_director_from_clinic.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('clinics', function (Blueprint $table) {
+            $table->dropColumn('technical_director');
+            $table->dropColumn('technical_director_document');
+            $table->dropColumn('technical_director_professional_id');
+            $table->dropColumn('parent_clinic_id');
+            $table->dropColumn('address');
+            $table->dropColumn('city');
+            $table->dropColumn('state');
+            $table->dropColumn('postal_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+    }
+};
