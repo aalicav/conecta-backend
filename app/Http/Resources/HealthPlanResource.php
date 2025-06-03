a943cc8da749036699dfd1f05f2c1032e3396324
aalicav <antonioalissoncavalcantedeoliv@gmail.com>
2025-06-03 13:19:53 -0300

    Update HealthPlanResource to use entity_id instead of id for improved data consistency

--- app/Http/Resources/HealthPlanResource.php
+++ app/Http/Resources/HealthPlanResource.php
@@ -16,7 +16,7 @@ class HealthPlanResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
+            'id' => $this->entity_id,
             'name' => $this->name,
             'cnpj' => $this->cnpj,
             'ans_code' => $this->ans_code,
